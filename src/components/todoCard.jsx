21ab8457d296e0d8e7fd3a142ec80893c681213a
Lavish Agarwal <agrwl.lavish@gmail.com>
2025-07-20 13:50:58 +0530

    Project Setup

---
+++ src/components/todoCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function todoCard() {
+  return (
+    <div>
+        <input type="checkbox" name="" id="" />
+        <input type="text" name="" id="" />
+        <button>Delete</button>
+        <button>Edit</button>
+    </div>
+  )
+
+export default todoCard
